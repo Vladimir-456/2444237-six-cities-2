df5a189f5dfd3205e089c036445a58a13d746d52
Your Name <your.email@example.com>
2026-03-29 13:48:10 +0300

    Разработка сервисов для работы с базой данных

--- src/shared/modules/user/user-service.ts
+++ src/shared/modules/user/user-service.ts
@@ -1,18 +1,23 @@
-import { DocumentType } from '@typegoose/typegoose';
-import { CreateUserDto } from './dto/user-dto.js';
-import { UserServiceInterface } from './user-service.interface.js';
-import { UserEntity, UserModel } from './user.entity.js';
-import { inject, injectable } from 'inversify';
-import { Component } from '../../types/container.js';
-import { Logger } from '../../libs/logger/index.js';
+import { DocumentType } from "@typegoose/typegoose";
+import { CreateUserDto } from "./dto/user-dto.js";
+import { UserServiceInterface } from "./user-service.interface.js";
+import { UserEntity, UserModel } from "./user.entity.js";
+import { inject, injectable } from "inversify";
+import { Component } from "../../types/container.js";
+import { Logger } from "../../libs/logger/index.js";
+import { OfferEntity, OfferModel } from "../offer/offer.entity.js";
 
 @injectable()
 export class UserService implements UserServiceInterface {
   constructor(
-    @inject(Component.Logger) private readonly logger: Logger
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.UserModel) private readonly userModel: typeof UserModel,
   ) {}
 
-  async register(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
+  async register(
+    dto: CreateUserDto,
+    salt: string,
+  ): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
@@ -23,14 +28,42 @@ export class UserService implements UserServiceInterface {
   }
 
   async findByEmail(email: string): Promise<DocumentType<UserEntity> | null> {
-    const result = await UserModel.findOne({ email });
-    this.logger.info(`Find user by email: ${email}`);
-    return result;
+    return this.userModel.findOne({ email }).exec();
   }
 
   async findById(id: number): Promise<DocumentType<UserEntity> | null> {
-    const result = await UserModel.findById(id);
-    this.logger.info(`Find user by id: ${id}`);
-    return result;
+    return this.userModel.findById(id).exec();
+  }
+
+  async findFavoriteOffers(
+    userId: number,
+  ): Promise<DocumentType<OfferEntity>[]> {
+    const user = await UserModel.findById(userId).populate("favorites").exec();
+
+    return (user?.favorites as DocumentType<OfferEntity>[]) ?? [];
+  }
+
+  async updateAndDeleteFavoriteOffer(
+    offerId: string,
+    userId: number,
+  ): Promise<DocumentType<OfferEntity> | null> {
+    const user = await UserModel.findById(userId).populate("favorites").exec();
+    const offer = await OfferModel.findById(offerId).exec();
+    if (!user) {
+      return null;
+    }
+    const index = user.favorites.findIndex((id) => id.toString() === offerId);
+
+    if (index === -1) {
+      await UserModel.findByIdAndUpdate(userId, {
+        $push: { favorites: offerId },
+      });
+    } else {
+      await UserModel.findByIdAndUpdate(userId, {
+        $pull: { favorites: offerId },
+      });
+    }
+
+    return offer;
   }
 }
